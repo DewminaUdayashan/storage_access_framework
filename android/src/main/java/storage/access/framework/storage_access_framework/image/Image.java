7e00295291db16af6767a7da174033afe5616146
DewminaUdayashan <dewminaudayashan@gmail.com>
2021-08-28 23:00:39 +0530

    Memory Out Fix

--- android/src/main/java/storage/access/framework/storage_access_framework/image/Image.java
+++ android/src/main/java/storage/access/framework/storage_access_framework/image/Image.java
@@ -41,91 +41,45 @@ public class Image {
 //            }
 
             for (DocumentFile file : files) {
-
                 try {
+                    InputStream inputStream = context.getContentResolver().openInputStream(file.getUri());
+
                     if (types.isEmpty()) {
-                        Log.d(TAG, "getImages: FILE EXTENSIONS EMPTY");
-                        InputStream inputStream = context.getContentResolver().openInputStream(file.getUri());
                         BufferedInputStream bis = new BufferedInputStream(inputStream);
                         ByteArrayOutputStream baos = new ByteArrayOutputStream();
-
                         byte buffer[] = new byte[1024];
                         int len;
                         while ((len = bis.read(buffer, 0, buffer.length)) > 0) {
                             baos.write(buffer, 0, len);
                         }
                         bis.close();
-
                         byte[] imageData = baos.toByteArray();
                         baos.close();
                         images.add(imageData);
-//                        InputStream iStream = context.getContentResolver().openInputStream(file.getUri());
-//                        byte[] inputData = getBytes(iStream, file.length());
-//                        Log.d(TAG, "getImages: IMAGE BYTES" + Arrays.toString(inputData));
-//                        images.add(inputData);
-
                     } else {
                         for (String type : types) {
-                            Log.d(TAG, "getImages: FILE EXTENSION => " + type);
                             if (Objects.requireNonNull(file.getName()).contains(type)) {
-//                                InputStream iStream = context.getContentResolver().openInputStream(file.getUri());
-//                                byte[] inputData = getBytes(iStream, file.length());
-//                                Log.d(TAG, "getImages: IMAGE BYTES" + Arrays.toString(inputData));
-//                                images.add(inputData);
-
-                                InputStream inputStream = context.getContentResolver().openInputStream(file.getUri());
                                 BufferedInputStream bis = new BufferedInputStream(inputStream);
                                 ByteArrayOutputStream baos = new ByteArrayOutputStream();
-
-                                byte buffer[] = new byte[1024];
+                                byte[] buffer = new byte[1024];
                                 int len;
                                 while ((len = bis.read(buffer, 0, buffer.length)) > 0) {
                                     baos.write(buffer, 0, len);
                                 }
                                 bis.close();
-
                                 byte[] imageData = baos.toByteArray();
                                 baos.close();
                                 images.add(imageData);
-
                             }
                         }
                     }
-
-//                    if (Objects.requireNonNull(file.getName()).contains(".jpg") || Objects.requireNonNull(file.getName()).contains(".png") || Objects.requireNonNull(file.getName()).contains(".jpeg")) {
-//                        InputStream iStream = context.getContentResolver().openInputStream(file.getUri());
-//                        byte[] inputData = getBytes(iStream);
-//                        Log.d(TAG, "getImages: IMAGE BYTES" + Arrays.toString(inputData));
-//                        images.add(inputData);
-//                    }
                 } catch (Exception e) {
                     Log.d(TAG, "getImages: Exception Occurred => " + e);
                 }
 
             }
         }
-        Log.d(TAG, "getImages: IMAGES LEN BEFORE SEND " + images.size());
         return images;
     }
-
-    static ByteArrayOutputStream byteBuffer = new ByteArrayOutputStream();
-    static int bufferSize = 1024;
-    static int len = 0;
-
-
-    private static byte[] getBytes(InputStream inputStream, long length) throws IOException {
-        byteBuffer.reset();
-        len = 0;
-        bufferSize = 1024;
-        if (length > bufferSize)
-            bufferSize = (int) length;
-        byte[] buffer = new byte[bufferSize];
-        while ((len = inputStream.read(buffer)) != -1) {
-            byteBuffer.write(buffer, 0, len);
-        }
-        byteBuffer.close();
-        return byteBuffer.toByteArray();
-    }
-
 
 }
